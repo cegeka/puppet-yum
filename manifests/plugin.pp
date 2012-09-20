9a850775b2168a01056b1189a7af2da42ea2af62
fdammeke <fabian.dammekens@cegeka.be>
2012-09-20 16:42:40 +0200

    Fix for redhat rhn-plugin

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -1,21 +1,14 @@
 define yum::plugin( $ensure = undef ) {
+  include yum::params
+
   if $ensure in [ present, absent ] {
     $ensure_real = $ensure
   }
   else {
     fail("Yum::Plugin[${title}]: parameter ensure must be present or absent")
   }
-  case $::operatingsystem {
-    redhat, centos: {
-      $pluginname = $::operatingsystemrelease ? {
-        /5.*/ => "yum-${title}",
-        /6.*/ => "yum-plugin-${title}",
-      }
-    }
-    default: { fail("${::operatingsystem} is not yet supported") }
-  }
 
-  package { $pluginname:
+  package { $yum::params::pluginname:
     ensure => $ensure_real,
   }
 }
