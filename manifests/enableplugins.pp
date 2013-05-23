358f4c3be3f79e4e439b155884aff882c9c061af
Bart Wouters <bart.wouters@cegeka.be>
2013-05-23 18:07:41 +0200

    cleanup .swp files

--- manifests/enableplugins.pp
+++ manifests/enableplugins.pp
@@ -2,7 +2,7 @@ define yum:enableplugins (ensure = 'present') {
   case $::operatingsystem {
     redhat: {
       augeas {"enable_plugins":
-        changes =>  [ "set /files/etc/yum.conf/plugins true"],
+        changes =>  [ "set /files/etc/yum.conf/main/plugins 1"],
     }
   }
 }
