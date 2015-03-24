b704ca6e41b27c13f06c1964f4b3d57fa7c4e698
fdammeke <fabian.dammekens@cegeka.be>
2015-03-24 17:17:25 +0100

    Allow a custom plugin config file to be defined

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -1,4 +1,4 @@
-define yum::plugin ($ensure = 'present', $enable = true) {
+define yum::plugin ($ensure = 'present', $enable = true, $config_file = undef) {
   case $::osfamily {
     redhat: {
       # Dirty hack because redhat doesn't follow conventions
@@ -12,6 +12,12 @@ define yum::plugin ($ensure = 'present', $enable = true) {
       }
     }
     default: { fail("only supported on osfamily RedHat") }
+  }
+
+  if ! $config_file {
+    $real_config_file = $title
+  } else {
+    $real_config_file = $config_file
   }
 
   if $ensure in [ present, absent, purged ] {
@@ -38,9 +44,9 @@ define yum::plugin ($ensure = 'present', $enable = true) {
 
   if $ensure_real == 'present' {
     augeas { "yum-plugin-${title}-enable":
-      incl    => "/etc/yum/pluginconf.d/${title}.conf",
+      incl    => "/etc/yum/pluginconf.d/${real_config_file}.conf",
       lens    => 'Yum.lns',
-      context => "/files/etc/yum/pluginconf.d/${title}.conf/main",
+      context => "/files/etc/yum/pluginconf.d/${real_config_file}.conf/main",
       changes => "set enabled ${_enable}",
       onlyif  => "match enabled[. = '${_enable}'] size == 0"
     }
